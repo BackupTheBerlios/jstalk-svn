36b1397f5502aa019a6ac5f22efb2dfcd8c78dc6
thiago_silva <thiago_silva@820d2df5-eb31-0410-8dc6-ede529566b30>
2007-07-04 18:31:19 +0000

    nomsg

---
+++ trunk/homepage/index.php
@@ -0,0 +1,30 @@
+<?php
+//   include("process_visitor.php");
+?>
+
+<html>
+  <title>JSTalk - JavaScript with Smalltalk tools</title>
+<body>
+  <h1>JSTalk</h1>
+
+  <p>JSTalk is a tool entirely written in JavaScript, using the <a href="http://www.mozilla.org/rhino/">Rhino</a>
+    implementation for the Java platform.
+    It provides a class-based core and the basic tools wich can be found in many Smalltalk environments.
+  </p>
+
+  <p>To run it, all you need is a java environment with Swing support and the Rhino JavaScript (or, simply Sun's JSDK 6,
+    wich comes with the binary JS shell "jrunscript").</p>
+
+<h3>Screenshots</h3>
+  <a href="ss1.png"><img src="ss1.png" width="200" height="125"></a>
+<h3>Download</h3>
+
+  <p>
+    <a href="https://developer.berlios.de/project/showfiles.php?group_id=8504">Version 0.3 (07/04/2007)</a>
+  </p>
+
+<hr>
+<p>Author: <a href="mailto:tsilva@sourcecraft.info">Thiago Silva</a></p>
+
+</body>
+</html>
