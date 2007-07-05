62b23e4f676440a33e9ae9376281a2fb0910d1b2
thiago_silva <thiago_silva@820d2df5-eb31-0410-8dc6-ede529566b30>
2007-07-05 01:53:36 +0000

    -tagging v0.3

---
+++ tags/jstalk_0_3/JSTalk.js
@@ -0,0 +1,8 @@
+load("core_extensions.js")
+load("core_loader.js")
+
+/* Basic JSTalk */
+$ClassLoader.load('JSObject')
+$ClassLoader.load('System', true)
+$ClassLoader.load('SystemDictionary', true)
+System.initialize()
